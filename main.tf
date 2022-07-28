844c7c2b354087d75fcf03e65fe1214f960996f0
Rahulkalal <107499198+Rahulkalal@users.noreply.github.com>
2022-07-28 23:40:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,9 +9,10 @@ terraform {
    }
 
 provider "aws" {
-        region = "us-east-1" 
-        #profile = "default"
-	}
+   region = "us-east-1"
+   access_key = "AKIA5EJUAEH2GHKMEJ6Z"
+   secret_key = "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"
+ }
 
 resource "aws_instance" "Raminfra" {
 	ami = "ami-0cff7528ff583bf9a" 
